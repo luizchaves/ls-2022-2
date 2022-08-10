45ea8c6c745d5a12ebe37de897d69fc05a4ee5e9
Luiz <lucachaves@gmail.com>
2022-08-10 13:15:32 -0300

    statements

---
+++ 01-statment/media.js
@@ -0,0 +1,42 @@
+
+function calcularMediaIf(nota1, nota2, nota3) {
+  const media = (nota1 + nota2 + nota3)/ 3;
+  
+  if (media >= 70) {
+    return 'Aprovado';
+  } else if( media >= 40) {
+    return 'Final';
+  } else {
+    return 'Reprovado';
+  }
+}
+
+console.log(calcularMediaIf(70, 70, 70));
+console.log(calcularMediaIf(40, 40, 40));
+console.log(calcularMediaIf(30, 30, 30));
+
+function calcularMediaSwitch(nota1, nota2, nota3) {
+  const media = (nota1 + nota2 + nota3)/ 3;
+
+  switch(true) {
+    case media >= 70:
+      return 'Aprovado';
+    case media >= 40:
+      return 'Final';
+    default:
+      return 'Reprovado';
+  }
+}
+
+console.log(calcularMediaSwitch(70, 70, 70));
+console.log(calcularMediaSwitch(40, 40, 40));
+console.log(calcularMediaSwitch(30, 30, 30));
+
+
+let resultado = '';
+
+for(let x = 0;x < 100; x++) {
+  resultado += x
+}
+
+console.log(resultado);
